7684e6a26d4d1d3ecf645332748e6d1493e6078f
C-Rodg <rodgersbc@gmail.com>
2018-04-10 15:08:17 -0700

    Updated webpack  dev profile to WP4

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,66 +9,62 @@ const OUTPUT_DIR = path.resolve(__dirname, 'dist');
 const defaultInclude = [SRC_DIR];
 
 module.exports = {
-    target: 'web'
-    entry: {
-        app: SRC_DIR + './index.js',
-        vendor: Object.keys(package.dependencies)
-    },
-    output: {
-        path: OUTPUT_DIR,
-        publicPath: '/',
-        filename: '[name].bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [{ loader: 'style-loader' }, { loader: 'css-loader'}],
-                include: defaultInclude
-            },
-            {
-                test: /\.jsx?$/,
-                use: [{ loader: 'babel-loader'}],
-                include: defaultInclude
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/,
-                use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]'}],
-                include: defaultInclude
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                use: [{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]'}],
-                include: defaultInclude
-            }
-        ]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.[hash].js',
-            minChunks(module) {
-                return module.context && module.context.indexOf('node_modules') >= 0;
-            }
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Validar Check-in',
-            filename: './index.html',
-            favicon: './src/assets/favicon.ico',
-            template: './src/index.html',
-            chunks: ['vendor', 'app']
-        })
-    ],
-    devtool: 'cheap-source-map',
-    devServer: {
-        contentBase: OUTPUT_DIR,
-        stats: {
-            colors: true,
-            chunks: false,
-            children: false
-        }
-    }
+	mode: 'development',
+	target: 'web',
+	entry: {
+		app: path.resolve(SRC_DIR, 'index.js'),
+		vendor: Object.keys(package.dependencies)
+	},
+	output: {
+		path: OUTPUT_DIR,
+		publicPath: '/',
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+				include: defaultInclude
+			},
+			{
+				test: /\.jsx?$/,
+				use: [{ loader: 'babel-loader' }],
+				include: defaultInclude
+			},
+			{
+				test: /\.(jpe?g|png|gif)$/,
+				use: [{ loader: 'file-loader?name=img/[name]__[hash:base64:5].[ext]' }],
+				include: defaultInclude
+			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				use: [
+					{ loader: 'file-loader?name=font/[name]__[hash:base64:5].[ext]' }
+				],
+				include: defaultInclude
+			}
+		]
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		}),
+		new HtmlWebpackPlugin({
+			title: 'Validar Check-in',
+			filename: './index.html',
+			favicon: './src/assets/favicon.ico',
+			template: './src/index.html',
+			chunks: ['vendor', 'app']
+		})
+	],
+	devtool: 'cheap-source-map',
+	devServer: {
+		contentBase: OUTPUT_DIR,
+		stats: {
+			colors: true,
+			chunks: false,
+			children: false
+		}
+	}
 };
